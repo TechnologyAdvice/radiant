06a63d36b7f0ce294f868207c904127349d1f087
athurman <athurman215@gmail.com>
2015-06-04 12:02:40 -0500

    attempt to hide dimmer on modal show

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
   // Pop Overs on Sidebar elements
   $('.ui.thin.sidebar .blue.item').popup();
   // Add Interaction History Modal Show
-  $('.ui.interaction.modal').modal('attach events', '.add.interaction', 'show');
+  $('.ui.interaction.modal').modal('attach events', '.add.interaction', 'show').modal('hide dimmer');
   $('.ui.project.log.modal').modal('attach events', '.add.project', 'show');
 });
