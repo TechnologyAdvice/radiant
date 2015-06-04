7f6295a32daff6195ed7b2ca3308e1363d6b5836
athurman <athurman215@gmail.com>
2015-06-04 08:07:40 -0500

    add modal js

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
   $('.ui.checkbox').checkbox();
   // Pop Overs on Sidebar elements
   $('.ui.thin.sidebar .blue.item').popup();
+  // Add Interaction History Modal Show
+  $('.ui.interaction.modal').modal('attach events', '.add.interaction', 'show');
+  $('.ui.project.log.modal').modal('attach events', '.add.project', 'show');
 });
