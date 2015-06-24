29bfb1900f3d77624512379ef0c3f7a867be9694
athurman <athurman215@gmail.com>
2015-06-24 16:39:46 -0500

    remove example directory

--- example/js/dashboard.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.ui.checkbox').checkbox();
-  // Pop Overs on Sidebar elements
-  $('.ui.thin.sidebar .blue.item').popup();
-  // Add Interaction History Modal Show
-  $('.ui.interaction.modal').modal('attach events', '.add.interaction', 'show').modal('hide dimmer');
-  $('.ui.project.log.modal').modal('attach events', '.add.project', 'show');
-});
