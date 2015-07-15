bacb8f58871d74b7eee1c9bb0278387ebd39ebcd
levithomason <me@levithomason.com>
2015-07-15 16:37:03 -0700

    add publish prompt

--- gulp/tasks/publish.js
+++ gulp/tasks/publish.js
@@ -2,9 +2,20 @@ var g = require('gulp-load-plugins')();
 var gulp = g.help(require('gulp'), require('../gulphelp'));
 var runSequence = require('run-sequence');
 var paths = require('../paths');
-var pkg = require('../../package.json');
 
 gulp.task('publish', 'publish theme to cdn', function(cb) {
+  runSequence(
+    [
+      'build',
+      'docs'
+    ],
+    'upload-to-s3',
+    cb
+  )
+});
+
+gulp.task('upload-to-s3', function(cb) {
+  var pkg = require('../../package.json');
   var AWS_ACCESS_KEY_ID = process.env.RADIANT_AWS_ACCESS_KEY_ID;
   var AWS_SECRET_ACCESS_KEY = process.env.RADIANT_AWS_SECRET_ACCESS_KEY;
 
@@ -16,9 +27,11 @@ gulp.task('publish', 'publish theme to cdn', function(cb) {
     throw new Error('Missing RADIANT_AWS_SECRET_ACCESS_KEY. See README.md');
   }
 
+  var bucket = 'ta-radiant-assets';
+
   var publisher = g.awspublish.create({
     params: {
-      Bucket: 'ta-radiant-assets'
+      Bucket: bucket
     },
     accessKeyId: AWS_ACCESS_KEY_ID,
     secretAccessKey: AWS_SECRET_ACCESS_KEY
@@ -26,13 +39,20 @@ gulp.task('publish', 'publish theme to cdn', function(cb) {
 
   var headers = {};
 
+  var prefix = '/' + pkg.version;
+  
+  var confirmUploadMessage = [
+    'Upload build to S3 ',
+    g.util.colors.yellow(bucket + prefix)
+  ].join('');
+  
   return gulp.src(paths.dist + '/**/*')
+    .pipe(g.prompt.confirm(confirmUploadMessage))
+
     // prefix S3 objects with package.json version number 
     .pipe(g.rename(function(path) {
-      path.dirname = '/' + pkg.version + '/' + path.dirname;
+      path.dirname = prefix + '/' + path.dirname;
     }))
-
-    .pipe(g.debug())
 
     // gzip, Set Content-Encoding headers and add .gz extension 
     .pipe(g.awspublish.gzip({ext: '.gz'}))
@@ -42,7 +62,7 @@ gulp.task('publish', 'publish theme to cdn', function(cb) {
     .pipe(publisher.publish(headers))
 
     // create a cache file to speed up consecutive uploads 
-    //.pipe(publisher.cache())
+    .pipe(publisher.cache())
 
     // print upload updates to console 
     .pipe(g.awspublish.reporter());
