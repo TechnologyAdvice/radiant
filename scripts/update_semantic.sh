f7cd7febe8818dd6f7428994c9a9fae3e5160d71
levithomason <me@levithomason.com>
2015-11-17 14:16:18 -0800

    add update_semantic script

---
+++ scripts/update_semantic.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+##############################################################################
+# Update Semantic UI
+#
+#   This script will npm install the latest Semantic UI LESS and copy all the
+#   necessary files into our build.
+
+# exit immediately if any step fails
+set -e
+
+# get latest semantic
+npm i semantic-ui-less@latest -S
+
+# themes
+rm -rf src/themes/*
+cp -R node_modules/semantic-ui-less/themes/* src/themes/default
+
+# definitions
+rm -rf src/definitions/*
+cp -R node_modules/semantic-ui-less/definitions/* src/definitions
+
+# src/ root files
+cp node_modules/semantic-ui-less/semantic.less src
+cp node_modules/semantic-ui-less/theme.less src
+cp node_modules/semantic-ui-less/theme.config.example src
+mv src/theme.config.example src/theme.config
