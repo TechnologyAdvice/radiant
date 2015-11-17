0df097c43db064f84346fcd92b729d8b37c913aa
levithomason <me@levithomason.com>
2015-11-17 14:25:48 -0800

    echo steps in update script

--- scripts/update_semantic.sh
+++ scripts/update_semantic.sh
@@ -10,18 +10,24 @@
 set -e
 
 # get latest semantic
+echo "... updating Semantic UI NPM module"
 npm i semantic-ui-less@latest -D
 
 # themes
+echo "... copying themes"
 rm -rf src/themes/*
 cp -R node_modules/semantic-ui-less/themes/* src/themes/
 
 # definitions
+echo "... copying definitions"
 rm -rf src/definitions/*
 cp -R node_modules/semantic-ui-less/definitions/* src/definitions
 
 # src/ root files
+echo "... copying root files"
 cp node_modules/semantic-ui-less/semantic.less src
 cp node_modules/semantic-ui-less/theme.less src
 cp node_modules/semantic-ui-less/theme.config.example src
 mv src/theme.config.example src/theme.config
+
+echo "... Done!"
